a5481f8b99d86ba91bd5be5d353dc73dc05e234b
AndreAguilar1997 <91272416+AndreAguilar1997@users.noreply.github.com>
2021-12-06 07:52:46 -0400

    Add files via upload

---
+++ calculadora.hs
@@ -0,0 +1,11 @@
+--calcu;adora
+
+sumados x y = x+y
+--resta
+resta x y =x-y
+--multiplicacion
+multiplicacion x y =x*y
+--dividir
+divide x y = if (x `mod` y)==0
+then "si se puede"
+else "no se puede"
